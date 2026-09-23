7688df72e9be3a817018db946edff0e8091b72b9
farah246 <farahcherif619@gmail.com>
2026-09-23 15:16:41 +0100

    first commit

---
+++ src/main/java/rt4/example/Main.java
@@ -0,0 +1,7 @@
+package rt4.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/rt4/example/Product.java
@@ -0,0 +1,13 @@
+package rt4.example;
+
+public class Product {
+    private String type;
+    private String name;
+    private double price;
+    public Product(String type, String name, double price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+    // getters...
+}
